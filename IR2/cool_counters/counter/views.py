a6900b9ed9b770becb999ceb99c60f9cf45db7b1
himanshu-fynder <himanshu.agrawal@nucleusTeq.com>
2023-04-24 03:02:12 -0700

    test chanegs

--- IR2/cool_counters/counter/views.py
+++ IR2/cool_counters/counter/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Counter
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 def index(request):
-    if len(Counter.objects.filter(key='counter')) == 0:
-        counter = Counter(key='counter', value=0)
-        counter.save()
-    else:
-        counter = get_object_or_404(Counter, key='counter')
-    
-    counter.value+=1
-    counter.save()
-    context = {'value': counter.value}
-    return render(request, 'counter/index.html', context)
+	q = request.GET.get("q", None)
+	try: 
+		value = None 
+		if(q=='apple watch series 8'):
+			value = ["https://www.apple.com/apple-watch-series-8/",'https://www.apple.com/us/shop/goto/buy_watch/apple_watch_series_8','https://www.apple.com/',
+			'https://www.apple.com/#footnote-1']
+		elif(q=='airpods 2nd generation'):
+			value = ['https://www.apple.com/airpods/','https://www.apple.com/ipad/','https://www.apple.com/ipad-10.9/','https://www.apple.com/us/shop/goto/buy_ipad/ipad']
+		elif(q.strip()==''):
+			value = ['NOT FOUND']
+		else: 
+			value = ["https://www.apple.com/",'https://www.apple.com/us/shop/goto/buy_watch/apple_watch_series_8','https://www.apple.com/#footnote-1','https://www.apple.com/iphone-14/']
+		context = {'statuses':value }
+		return render(request, 'counter/index.html', context)
 
+	except: 
+		return render(request, 'counter/notfound.html', context)
